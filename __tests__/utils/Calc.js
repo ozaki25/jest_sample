17b3c4bcd0f317b57df4df1b35c7d300cbf30edc
ozaki <ozaki@oz.jp>
2017-10-03 09:03:44 +0900

    describeを使うようにした

--- __tests__/utils/Calc.js
+++ __tests__/utils/Calc.js
@@ -3,34 +3,36 @@ import React from 'react';
 import { add } from '../../src/utils/Calc';
 import renderer from 'react-test-renderer';
 
-test('add nonnegative integer', () => {
-  const val1 = 1;
-  const val2 = 1;
-  const result = add(val1, val2);
-  const expected = val1 + val2;
-  expect(expected).toBe(result);
-});
+describe('add two numbers', () => {
+  test('both positive numbers', () => {
+    const val1 = 1;
+    const val2 = 1;
+    const result = add(val1, val2);
+    const expected = val1 + val2;
+    expect(expected).toBe(result);
+  });
 
-test('add nonnegative integer and negative integer', () => {
-  const val1 = 1;
-  const val2 = -1;
-  const result = add(val1, val2);
-  const expected = val1 + val2;
-  expect(expected).toBe(result);
-});
+  test('positive number and negative number', () => {
+    const val1 = 1;
+    const val2 = -1;
+    const result = add(val1, val2);
+    const expected = val1 + val2;
+    expect(expected).toBe(result);
+  });
 
-test('add negative integer', () => {
-  const val1 = -1;
-  const val2 = -1;
-  const result = add(val1, val2);
-  const expected = val1 + val2;
-  expect(expected).toBe(result);
-});
+  test('both negative numbers', () => {
+    const val1 = -1;
+    const val2 = -1;
+    const result = add(val1, val2);
+    const expected = val1 + val2;
+    expect(expected).toBe(result);
+  });
 
-test('add zero', () => {
-  const val1 = 0;
-  const val2 = 0;
-  const result = add(val1, val2);
-  const expected = val1 + val2;
-  expect(expected).toBe(result);
+  test('both zero', () => {
+    const val1 = 0;
+    const val2 = 0;
+    const result = add(val1, val2);
+    const expected = val1 + val2;
+    expect(expected).toBe(result);
+  });
 });
